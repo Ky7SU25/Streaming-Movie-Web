dc745a42c6e1e8148afe7816f1be80ee64cce568
tqloi <quyloivn@gmail.com>
2025-05-29 11:57:23 +0700

    Add User UI

---
+++ StreamingMovie.Web/Views/Movie/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StreamingMovie.Domain.Entities;
+
+namespace StreamingMovie.Web.Views.Movie.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+
+        public CategoryController(SignInManager<User> signInManager, UserManager<User> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Details(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+        public IActionResult Matching(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+    }
+}
