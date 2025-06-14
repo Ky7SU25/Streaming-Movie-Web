53f9651e663b17bc741db760e20c0586bff1a379
thaiphuc1 <thaiphuc6104@gmail.com>
2025-06-14 23:03:30 +0700

    add admin template

---
+++ StreamingMovie.Web/Views/Admin/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StreamingMovie.Domain.Entities;
+
+namespace StreamingMovie.Web.Views.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+
+        public AdminController(SignInManager<User> signInManager, UserManager<User> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+        public IActionResult Billing(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+        public IActionResult Profile(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+        public IActionResult Table(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+    }
+}
