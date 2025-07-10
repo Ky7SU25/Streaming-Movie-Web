b722a718fb38f386e0292a8dbc94c61fa7b771bf
ngdatdev <nvdatdz8b@gmail.com>
2025-07-11 00:17:32 +0700

    Jenkins_8

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,8 @@ if docker compose pull; then
     echo "$CURRENT_IMAGE" > "$PREV_IMAGE_FILE"
 
     echo "==> Starting new container"
-    docker compose up -d --force-recreate
+    docker compose down
+    docker compose up -d
 else
     echo "==> Failed to pull. Rolling back…"
     if [ -f "$PREV_IMAGE_FILE" ]; then
