917698764861490454a6c2b8ebc3c0520ab674e3
Tran Dinh Gia Huy <trandinhgiahuy2004@gmail.com>
2025-07-24 00:01:22 +0700

    add email register template

--- StreamingMovie.Web/Views/Account/Controllers/AccountController.cs
+++ StreamingMovie.Web/Views/Account/Controllers/AccountController.cs
@@ -120,11 +120,20 @@ public class AccountController : Controller
                     returnUrl = returnUrl
                 }, protocol: Request.Scheme);
 
+                var root = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+                var templatePath = Path.Combine(root, "StreamingMovie.Domain", "Common", "Templates", "ConfirmEmailTemplate.html");
+
+                string htmlTemplate = await System.IO.File.ReadAllTextAsync(templatePath);
+
+                string emailBody = htmlTemplate
+                .Replace("{{ .ConfirmationURL }}", callbackUrl)
+                .Replace("{{ .Email }}", model.Email);
+
                 var mailContent = new MailContent
                 {
                     To = model.Email,
-                    Subject = "Confirm your email",
-                    Body = $"Please confirm your account by <a href='{(callbackUrl)}'>clicking here</a>."
+                    Subject = "Xác nhận tài khoản CineStream",
+                    Body = emailBody
                 };
                 await _mailService.SendMailAsync(mailContent);
                 TempData["RegisterSuccess"] = "Register successfully, please check your email and click in confirmation link to complete.";
