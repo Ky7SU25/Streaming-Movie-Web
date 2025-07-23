849bb6a4f6bf5bc57c3e89fe951437cb59694c8b
tqloi <quyloivn@gmail.com>
2025-07-24 04:18:41 +0700

    review-phim

--- StreamingMovie.Application/Mappings/CommentProfile.cs
+++ StreamingMovie.Application/Mappings/CommentProfile.cs
@@ -8,15 +8,20 @@ namespace StreamingMovie.Application.Mappings
     {
         public CommentProfile()
         {
-            CreateMap<Rating, RatingRequestDTO>()
-               .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<RatingRequestDTO, Rating>()
+                .ForMember(dest => dest.UserId, opt => {
+                    opt.PreCondition(src => src.UserId.HasValue);
+                    opt.MapFrom(src => src.UserId.Value);
+                })
+                .ForAllMembers(opt =>
+                    opt.Condition((src, dest, srcMember) => srcMember != null));
 
             CreateMap<Rating, RatingResponseDTO>()
                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.UserName))
                .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.User.FullName))
                .ForMember(dest => dest.AvatarUrl, opt => opt.MapFrom(src => src.User.AvatarUrl));
 
-            CreateMap<Comment, CommentRequestDTO>()
+            CreateMap<CommentRequestDTO, Comment>()
                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
 
             CreateMap<Comment, CommentResponseDTO>()
