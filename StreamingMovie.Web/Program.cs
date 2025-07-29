b64f2591a4e96e95f674831afa229ef473185b77
ngdatdev <nvdatdz8b@gmail.com>
2025-07-29 23:20:36 +0700

    Oke

--- StreamingMovie.Web/Program.cs
+++ StreamingMovie.Web/Program.cs
@@ -1,10 +1,11 @@
-﻿using StreamingMovie.Application.Interfaces;
+﻿using System;
+using Microsoft.AspNetCore.HttpOverrides;
+using StreamingMovie.Application.Interfaces;
 using StreamingMovie.Application.Services;
 using StreamingMovie.Application.Services.BackgroundServices;
 using StreamingMovie.Infrastructure.Data;
 using StreamingMovie.Infrastructure.Extensions;
 using StreamingMovie.Infrastructure.ExternalServices.Hubs;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +21,6 @@ builder.Services.AddCoreInfrastructure(builder.Configuration);
 //background service
 builder.Services.AddHostedService<SubscriptionCheckService>();
 
-
 // Add new video upload services
 builder.Services.AddScoped<IVideoUploadService, VideoUploadService>();
 builder.Services.AddScoped<IFileUploadService, FileUploadService>();
@@ -30,12 +30,13 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.LoginPath = "/Account/Login";
     options.AccessDeniedPath = "/Account/AccessDenied";
 
-    options.ExpireTimeSpan = TimeSpan.FromDays(14); 
-    options.SlidingExpiration = true; 
+    options.ExpireTimeSpan = TimeSpan.FromDays(14);
+    options.SlidingExpiration = true;
 
     options.Cookie.IsEssential = true;
+    options.Cookie.SameSite = SameSiteMode.None;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
 });
-
 
 var app = builder.Build();
 
@@ -54,7 +55,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
-
+app.UseForwardedHeaders(
+    new ForwardedHeadersOptions
+    {
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+    }
+);
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
